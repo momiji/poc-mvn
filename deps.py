612b4358cdaf74ecaf8eee5a1b4377ba4faa43e2
a443939 <christian.bourgeois@socgen.com>
2024-10-22 23:01:41 +0200

    wip

--- deps.py
+++ deps.py
@@ -1,4 +1,4 @@
-import argparse, os, sys
+import argparse, os
 
 # parse argumnes
 parser = argparse.ArgumentParser(description='Process some integers.')
@@ -6,23 +6,24 @@ parser.add_argument('--color', choices=['auto', 'never', 'always'], default='aut
 parser.add_argument('-b', '--basic', action="store_true", help='For basic tree output')
 parser.add_argument('-s', '--sections', default='collect', help='Print only sections: project|proj, properties|props, managements|mgts, dependencies|deps, collect|coll, tree (comma separated)')
 parser.add_argument('-m', '--modules', help="Print only modules in format 'module,module,...'")
-parser.add_argument('-p', '--trace-poms', action="store_true", help='Trace poms')
+parser.add_argument('--trace-poms', action="store_true", help='Trace poms')
 parser.add_argument('-d', '--trace-deps', help='Trace dependencies in format "groupId:artifactId,groupId:artifactId,..."')
-parser.add_argument('--trace-props', help='Trace properties in format "name,name,..."')
-parser.add_argument('--trace-ranges', action="store_true", help='Trace ranges computation')
+parser.add_argument('-p', '--trace-props', help='Trace properties in format "name,name,..."')
+parser.add_argument('-r', '--trace-ranges', action="store_true", help='Trace ranges computation')
 parser.add_argument('-v', '--verbose', action="store_true", help='Trace verbose mode')
 parser.add_argument('file', nargs='?', default='pom.xml', type=str, help='pom.xml file location')
+parser.add_argument('-w', '--width', type=int, default=150, help='Width of the first colomn')
 
 args = parser.parse_args()
 
 color = os.isatty(1) if args.color == 'auto' else True if args.color == 'always' else False
 sections = [ s.strip() for s in args.sections.split(',') ] if args.sections else None
 modules = [ m.strip() for m in args.modules.split(',') ] if args.modules else None
+width = args.width
 
 # tracer
-trace = False
-
 import pom_tracer
+trace = False
 tracer = pom_tracer.Tracer().set_debug(args.verbose)
 if args.trace_poms:
     tracer.set_poms(True)
@@ -40,12 +41,21 @@ if args.trace_props:
 if args.trace_ranges:
     tracer.set_ranges(True)
     trace = True
+if trace:
+    pom_tracer.TRACER = tracer
 
 # imports
 from pom_loader import load_pom_from_file, register_pom_locations
 from pom_solver import resolve_pom
 from pom_printer import print_pom
 from pom_struct import PomProperties
+
+def separator(s):
+    # print separator
+    print("#" * width)
+    print(f"# {s} ".ljust(width - 1, " ") + "#")
+    print("#" * width)
+
 
 # it is needed to manually register all pom not located in M2 repository
 # so they can be found even if their properties are not resolved
@@ -55,9 +65,10 @@ register_pom_locations(args.file)
 def print_files(file, module = None):
     pom = load_pom_from_file(file)
     assert pom
-    resolve_pom(pom, load_mgts = True, load_deps = True) #, initialProps = initialProps)
     if modules is None or module:
-        print_pom(pom, color = color, basic = args.basic, sections = sections)
+        separator(pom.fullname2())
+        resolve_pom(pom, load_mgts = True, load_deps = True) #, initialProps = initialProps)
+        print_pom(pom, color = color, basic = args.basic, sections = sections, indent = width)
 
     for module in pom.modules:
         if modules is None or module in modules:
